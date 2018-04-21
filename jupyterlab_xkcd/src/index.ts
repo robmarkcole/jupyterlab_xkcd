f37cc7b513c31ad735dee384c82dab2fa1a7e05f
robmarkcole <robmarkcole@gmail.com>
2018-04-21 15:26:37 +0100

    Add styling, attribution

--- jupyterlab_xkcd/src/index.ts
+++ jupyterlab_xkcd/src/index.ts
@@ -16,49 +16,59 @@ import '../style/index.css';
 /**
  * Initialization data for the jupyterlab_xkcd extension.
  */
-const extension: JupyterLabPlugin<void> = {
-  id: 'jupyterlab_xkcd',
-  autoStart: true,
-  requires: [ICommandPalette],
-  activate: (app: JupyterLab, palette: ICommandPalette) => {
-    console.log('JupyterLab extension jupyterlab_xkcd is activated!');
+ const extension: JupyterLabPlugin<void> = {
+   id: 'jupyterlab_xkcd',
+   autoStart: true,
+   requires: [ICommandPalette],
+   activate: (app: JupyterLab, palette: ICommandPalette) => {
+     console.log('JupyterLab extension jupyterlab_xkcd is activated!');
 
-    // Create a single widget
-    let widget: Widget = new Widget();
-    widget.id = 'xkcd-jupyterlab';
-    widget.title.label = 'xkcd.com';
-    widget.title.closable = true;
+     // Create a single widget
+     let widget: Widget = new Widget();
+     widget.id = 'xkcd-jupyterlab';
+     widget.title.label = 'xkcd.com';
+     widget.title.closable = true;
+     widget.addClass('jp-xkcdWidget'); // new line
+     // Add an image element to the panel
+     let img = document.createElement('img');
+     img.className = 'jp-xkcdCartoon'; // new line
+     widget.node.appendChild(img);
 
-    // Add an image element to the panel
-    let img = document.createElement('img');
-    widget.node.appendChild(img);
+     // New: add an attribution badge
+     img.insertAdjacentHTML('afterend',
+       `<div class="jp-xkcdAttribution">
+         <a href="https://creativecommons.org/licenses/by-nc/2.5/" class="jp-xkcdAttribution" target="_blank">
+           <img src="https://licensebuttons.net/l/by-nc/2.5/80x15.png" />
+         </a>
+       </div>`
+     );
 
-    // Fetch info about a random comic
-    fetch('https:////egszlpbmle.execute-api.us-east-1.amazonaws.com/prod').then(response => {
-      return response.json();
-    }).then(data => {
-      img.src = data.img;
-      img.alt = data.title;
-      img.title = data.alt;
-    });
+     // Fetch info about a random comic
+     fetch('https:////egszlpbmle.execute-api.us-east-1.amazonaws.com/prod').then(response => {
+       return response.json();
+     }).then(data => {
+       img.src = data.img;
+       img.alt = data.title;
+       img.title = data.alt;
+     });
 
-    // Add an application command
-    const command: string = 'xkcd:open';
-    app.commands.addCommand(command, {
-      label: 'Random xkcd comic',
-      execute: () => {
-        if (!widget.isAttached) {
-          // Attach the widget to the main work area if it's not there
-          app.shell.addToMainArea(widget);
-        }
-        // Activate the widget
-        app.shell.activateById(widget.id);
-      }
-    });
+     // Add an application command
+     const command: string = 'xkcd:open';
+     app.commands.addCommand(command, {
+       label: 'Random xkcd comic',
+       execute: () => {
+         if (!widget.isAttached) {
+           // Attach the widget to the main area if it's not there
+           app.shell.addToMainArea(widget);
+         }
+         // Activate the widget
+         app.shell.activateById(widget.id);
+       }
+     });
 
-    // Add the command to the palette.
-    palette.addItem({command, category: 'Tutorial'});
-  }
-}
+     // Add the command to the palette.
+     palette.addItem({command, category: 'Tutorial'});
+   }
+ };
 
-export default extension;
+ export default extension;
